738cd65388fb5502840c21a6a58f6ce49381c844
Koakuma <koachan@protonmail.com>
2018-06-22 18:21:21 +0700

    Add Linux/PPC support

--- vendor/golang.org/x/net/internal/socket/sys_linux_ppc.go
+++ vendor/golang.org/x/net/internal/socket/sys_linux_ppc.go
@@ -2,13 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build arm mips mipsle 386 ppc
-// +build linux
-
 package socket
 
-func (h *cmsghdr) set(l, lvl, typ int) {
-	h.Len = uint32(l)
-	h.Level = int32(lvl)
-	h.Type = int32(typ)
-}
+const (
+	sysRECVMMSG = 343
+	sysSENDMMSG = 349
+)
